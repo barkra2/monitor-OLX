2f736651f8f4f5100178b4ffabdcf783dcbfa31c
maskonurek2 <bartlomiejkrasniak@gmail.com>
2026-04-15 21:36:59 +0200

    dzialajca baza danych

--- database.py
+++ database.py
@@ -11,6 +11,32 @@ def parse_price(raw: str) -> float | None:
         return None
     return float(numbers.replace(",", "."))
 
+def parse_date(raw: str):
+    DATY = {
+        "stycznia": "01",
+        "lutego": "02",
+        "marca": "03",
+        "kwietnia": "04",
+        "maja": "05",
+        "czerwca": "06",
+        "lipca": "07",
+        "sierpnia": "08",
+        "września": "09",
+        "października": "10",
+        "listopada": "11",
+        "grudnia": "12"
+    }
+    data = ""
+    for i in DATY:
+        if raw.find(i) > -1:
+            data = raw.replace(i, DATY[i])
+            data = data[-10:].replace(" ", "-")
+        if raw.find("Dzisiaj") > -1 or raw.find("dzisiaj") > -1:
+            data = str(datetime.date.today())
+            data = f"{data[8:]}-{data[5:7]}-{data[:4]}"
+    return data
+            
+
 with sqlite3.connect("baza_danych.db") as conn:
 
     cursor = conn.cursor()
@@ -21,7 +47,7 @@ with sqlite3.connect("baza_danych.db") as conn:
                     tytul TEXT NOT NULL,
                     cena FLOAT,
                     lokalizacja TEXT,
-                    data TEXT, 
+                    data DATE, 
                     url TEXT NOT NULL
                 )
                 """)
@@ -35,10 +61,8 @@ with sqlite3.connect("baza_danych.db") as conn:
         location = location_and_date[:location_and_date.rfind("-")]
         date = location_and_date[location_and_date.rfind("-")+2:]
         if price is not None and price > 400:
-            if date.find("Dzisiaj") > -1:
-                date = f"{date[-5:]} {datetime.date.today()}"
-            # if date.find("Odświeżono"):
-            #     date = date[date.find("a "):]
+            date = parse_date(date)
+
             cursor.execute("INSERT INTO produkty (tytul, cena, lokalizacja, data, url) VALUES (?, ?, ?, ?, ?)", (title, price, location, date, url))
 
     conn.commit()
